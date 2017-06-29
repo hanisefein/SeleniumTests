41580b5051c45d77514f8295707055a5fb2a1f6f
hanisefein <hanis@holidaytaxis.com>
2017-06-29 16:20:47 +0100

    deep links

--- src/Tests/Deep_Links/test_003_airport_guide_generated_from_affilitools.java
+++ src/Tests/Deep_Links/test_003_airport_guide_generated_from_affilitools.java
@@ -14,14 +14,14 @@ import java.util.concurrent.TimeUnit;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-public class test_003_airport_guide {
+public class test_003_airport_guide_generated_from_affilitools {
 
     String actual;
     String expected;
     String outcome;
 
     @Test
-    public void google_airport_without_route() {
+    public void airport_guide_generated_from_affilitools() {
 
         //Firefox
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\Hsefein\\Downloads\\geckodriver-v0.11.1-win32 (1)\\geckodriver.exe");
