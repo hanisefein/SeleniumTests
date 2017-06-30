fd92fa2e152be5d01fa2e5d9704bd0015f242e8f
hanisefein <hanis@holidaytaxis.com>
2017-06-30 16:08:22 +0100

    deep links

---
+++ src/Constants/Utils.java
@@ -0,0 +1,21 @@
+package Constants;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Utils {
+
+    public static void wait(int args)
+    {
+// WAIT IN SECONDS
+        try
+        {
+            TimeUnit.SECONDS.sleep(4);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
+}
