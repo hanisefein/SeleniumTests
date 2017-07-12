e11a1ffa31bed1bf547215ef58f1663a1bc14389
hanisefein <hanis@holidaytaxis.com>
2017-07-12 09:58:52 +0100

    homepage

--- src/Tests/Homepage/test_001_homepage_Sterling_to_EURO.java
+++ src/Tests/Homepage/test_001_homepage_Sterling_to_EURO.java
@@ -1,5 +1,6 @@
 package Tests.Homepage;
 
+import Constants.Utils;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,32 +12,30 @@ import java.util.concurrent.TimeUnit;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-public class test_001_homepage_Sterling_to_Euro {
+public class test_001_homepage_Sterling_to_EURO {
 
     String actual;
     String expected;
     String outcome;
 
     @Test
-    public void change_sterling_to_euro() {
+    public void all_currencies_sterling_to_us() {
 
         //Firefox
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Hsefein\\Downloads\\geckodriver-v0.11.1-win32 (1)\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Hsefein\\Downloads\\geckodriver-v0.17.0-win32\\geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
 
         try
         {
             // GET VERSION
-//            System.out.println("Version: " + AppVersion.AppVersion);
+//            System.out.println("Version: " + Constants.AppVersion);
 //            System.out.println();
 
             driver.navigate().to("http://public.htxdev.com/en/?ref=htx");
-            try {
-                TimeUnit.SECONDS.sleep(4);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+
+            // WAIT
+            Utils.wait(4);
 
             // VERIFY CURRENCY IS £ STERLING
             actual = driver.findElement(By.xpath("//*[@id=\"dropdownMenu2\"]/span[1]")).getText();
@@ -53,32 +52,23 @@ public class test_001_homepage_Sterling_to_Euro {
                 outcome = "FAIL";
             }
             System.out.println();
-            System.out.println(outcome + " Default Currency found is: " + actual);
-            System.out.println();
+            System.out.println("Found currency: " + actual + " : " + outcome);
 
             // SELECT CURRENCY US
             WebElement currency = driver.findElement(By.id("dropdownMenu2"));
             currency.click();
 
-            // WAIT 2 SECONDS
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+            // WAIT 3 SECONDS
+            Utils.wait(3);
 
-            // SELECT EURO
-            driver.findElement(By.xpath("//*[@id=\"example-navbar-collapse\"]/ul/li[2]/section/ul/li[2]/ul/li[3]")).click();
+            // SELECT US
+            driver.findElement(By.xpath("//*[@id=\"example-navbar-collapse\"]/ul/li[2]/section/ul/li[2]/ul/li[1]")).click();
 
             // WAIT 3 SECONDS
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+            Utils.wait(3);
 
             actual = driver.findElement(By.xpath("//*[@id=\"dropdownMenu2\"]/span[1]")).getText();
-            expected = "";
+            expected = "€";
 
             assertThat(actual, equalTo(expected));
 
@@ -90,11 +80,8 @@ public class test_001_homepage_Sterling_to_Euro {
             {
                 outcome = "FAIL";
             }
-
+            System.out.println("Found currency: " + actual + " : " + outcome);
             System.out.println();
-            System.out.println(outcome + " Currency changed to: " + actual);
-            System.out.println();
-
 
             if (outcome.equals("PASS"))
             {
@@ -110,9 +97,9 @@ public class test_001_homepage_Sterling_to_Euro {
         {
             System.out.println(e);
         }
-//        finally
-//        {
-//            driver.quit();
-//        }
+        finally
+        {
+            driver.quit();
+        }
     }
 }
